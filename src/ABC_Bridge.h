078c90d2d076bceba1e4e711979164d69bbaa641
Adam Harris <adam@dittylabs.com>
2014-03-19 13:07:08 -0700

    Modified prototype

--- src/ABC_Bridge.h
+++ src/ABC_Bridge.h
@@ -36,7 +36,7 @@ extern "C" {
 
     tABC_CC ABC_BridgeGetBitcoinPubAddress(char **pszPubAddress,
                                            tABC_U08Buf PrivateSeed,
-                                           unsigned int N,
+                                           int64_t N,
                                            tABC_Error *pError);
 
 #ifdef __cplusplus
