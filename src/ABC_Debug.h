29b43799976e17117be42798ed13abc1ccedd5d1
Adam Harris <adam@dittylabs.com>
2014-03-26 12:17:45 -0700

    Fixed formatting

--- src/ABC_Debug.h
+++ src/ABC_Debug.h
@@ -1,1 +1,30 @@
-/** * @file * AirBitz Debug function prototypes * * * @author Adam Harris * @version 1.0 */#ifndef ABC_Debug_h#define ABC_Debug_h#include <strings.h>#include "ABC.h"#ifdef __cplusplusextern "C" {#endif#ifdef DEBUG#define ABC_DEBUG(cmd)      cmd#else#define ABC_DEBUG(cmd)#endif     void ABC_DebugLog(const char * format, ...);#ifdef __cplusplus}#endif#endif
+/**
+ * @file
+ * AirBitz Debug function prototypes
+ *
+ *
+ * @author Adam Harris
+ * @version 1.0
+ */
+#ifndef ABC_Debug_h
+#define ABC_Debug_h
+#include <strings.h>
+#include "ABC.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef DEBUG
+#define ABC_DEBUG(cmd)      cmd
+#else
+#define ABC_DEBUG(cmd)
+#endif 
+    
+void ABC_DebugLog(const char * format, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
