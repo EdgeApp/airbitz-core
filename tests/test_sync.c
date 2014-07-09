0bf76ad78cf09105e2e55b35a4c24f26b9280dcb
William Swanson <swansontec@gmail.com>
2014-07-08 17:12:14 -0700

    Test repsitory creation

--- tests/test_sync.c
+++ tests/test_sync.c
@@ -1,4 +1,5 @@
 #include <ABC.h>
+#include <ABC_Sync.h>
 #include <ABC_Util.h>
 #include <errno.h>
 #include <stdio.h>
@@ -10,8 +11,13 @@ tABC_CC TestSync(tABC_Error *pError)
     tABC_CC cc = ABC_CC_Ok;
     int e;
 
+    ABC_CHECK_RET(ABC_SyncInit(pError));
+
     e = mkdir("sync_repo", S_IRWXU | S_IRWXG | S_IRWXO);
     ABC_CHECK_ASSERT(!e, ABC_CC_SysError, "mkdir failed");
+    ABC_CHECK_RET(ABC_SyncMakeRepo("sync_repo", pError));
+
+    ABC_SyncTerminate();
 
 exit:
     return cc;
