76ce4bd8d228abae6d13e5e88549bc76e367f8df
William Swanson <swansontec@gmail.com>
2014-07-18 12:19:03 -0700

    Expose more sync library details

--- src/sync.h
+++ src/sync.h
@@ -13,12 +13,28 @@ extern "C" {
 #endif
 
 /**
- * Syncs with a remote repository.
- * Performs a fetch, a file-system sync, and a push (if necessary).
- * @return 1 if files have changed on disk, 0 if the sync was successful
- * but produced no changes, or < 0 to indicate an error.
+ * Fetches the contents of the server into the "incoming" branch.
  */
-int sync_repo(git_repository *repo, const char *server);
+int sync_fetch(git_repository *repo,
+               const char *server);
+
+/**
+ * Updates the master branch with the latest changes, including local
+ * changes and changes from the remote repository.
+ * @param files_changed set to 1 if the function has changed the workdir.
+ * @param need_push set to 1 if the master branch has changes not on the
+ * server.
+ */
+int sync_master(git_repository *repo,
+                int *files_changed,
+                int *need_push);
+
+/**
+ * Pushes the master branch to the server.
+ */
+int sync_push(git_repository *repo,
+              const char *server);
+
 
 #ifdef __cplusplus
 }
