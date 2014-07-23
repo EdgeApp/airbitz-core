795adf2f6e3696ab886aa9b33d3566d84103cd72
William Swanson <swansontec@gmail.com>
2014-07-22 17:25:10 -0700

    util: Add create-account

---
+++ util/common.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Common utility functions for the command-line wallet interface
+ */
+
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#include <ABC.h>
+
+#define MAIN_CHECK(f) \
+    if (ABC_CC_Ok != (cc = f))\
+    { \
+        PrintError(cc, &error); \
+        return 1; \
+    }
+
+/**
+ * Examines a return code and prints an error if it is not OK.
+ */
+void PrintError(tABC_CC cc, tABC_Error *pError);
+
+#endif
