cc6d27c1b5cce472a7d90822185cb53301a8e8b8
William Swanson <swansontec@gmail.com>
2014-07-23 19:22:49 -0700

    util: Add settings commands

---
+++ util/set-nickname.c
@@ -0,0 +1,28 @@
+#include "common.h"
+#include "ABC_Util.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    tABC_CC cc;
+    tABC_Error error;
+    tABC_LoginSettings *pSettings = NULL;
+    unsigned char seed[] = {1, 2, 3};
+
+    if (argc != 5)
+    {
+        fprintf(stderr, "usage: %s <dir> <user> <pass> <nickname>\n", argv[0]);
+        return 1;
+    }
+
+    MAIN_CHECK(ABC_Initialize(argv[1], NULL, 0, seed, sizeof(seed), &error));
+    MAIN_CHECK(ABC_LoadAccountSettings(argv[2], argv[3], &pSettings, &error));
+    free(pSettings->szNickname);
+    pSettings->szNickname = strdup(argv[4]);
+    MAIN_CHECK(ABC_UpdateAccountSettings(argv[2], argv[3], pSettings, &error));
+
+    MAIN_CHECK(ABC_ClearKeyCache(&error));
+    ABC_FreeAccountSettings(pSettings);
+    return 0;
+}
