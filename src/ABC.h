3912a87f9fc05d17d67693f02d184a55c4efdf77
Paul Puey <paul@airbitz.co>
2014-07-26 13:09:05 -0700

    Further implement SMS/Email URIs. Better links but still doesn't work in all mail/sms apps.

--- src/ABC.h
+++ src/ABC.h
@@ -764,7 +764,7 @@ extern "C" {
                                       const char *szPassword,
                                       const char *szWalletUUID,
                                       const char *szRequestID,
-                                      unsigned char **pszURI,
+                                      char **pszURI,
                                       unsigned char **paData,
                                       unsigned int *pWidth,
                                       tABC_Error *pError);
