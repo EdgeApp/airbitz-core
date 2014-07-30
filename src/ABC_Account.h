45dba53ab8cda713a443ead52e8ad97d93613a7a
William Swanson <swansontec@gmail.com>
2014-07-30 16:31:07 -0700

    Add wallet features to the account

--- src/ABC_Account.h
+++ src/ABC_Account.h
@@ -13,6 +13,28 @@
 extern "C" {
 #endif
 
+    /**
+     * Account-level wallet structure.
+     *
+     * This structure contains the information stored for a wallet at thee
+     * account level.
+     */
+    typedef struct sABC_AccountWalletInfo
+    {
+        /** Unique wallet id. */
+        char *szUUID;
+        /** Bitcoin master seed. */
+        tABC_U08Buf BitcoinSeed;
+        /** The sync key used to access the server. */
+        tABC_U08Buf SyncKey;
+        /** The encryption key used to protect the contents. */
+        tABC_U08Buf MK;
+        /** Sort order. */
+        unsigned sortIndex;
+        /** True if the wallet should be hidden. */
+        bool archived;
+    } tABC_AccountWalletInfo;
+
     tABC_CC ABC_AccountCreate(tABC_SyncKeys *pKeys,
                               tABC_Error *pError);
 
@@ -38,6 +60,34 @@ extern "C" {
                                     tABC_Error *pError);
 
     void ABC_AccountSettingsFree(tABC_AccountSettings *pSettings);
+
+    void ABC_AccountWalletInfoFree(tABC_AccountWalletInfo *pInfo);
+    void ABC_AccountWalletInfoFreeArray(tABC_AccountWalletInfo *aInfo,
+                                        unsigned count);
+
+    tABC_CC ABC_AccountWalletList(tABC_SyncKeys *pKeys,
+                                  char ***paszUUID,
+                                  unsigned *pCount,
+                                  tABC_Error *pError);
+
+    tABC_CC ABC_AccountWalletsLoad(tABC_SyncKeys *pKeys,
+                                   tABC_AccountWalletInfo **paInfo,
+                                   unsigned *pCount,
+                                   tABC_Error *pError);
+
+    tABC_CC ABC_AccountWalletLoad(tABC_SyncKeys *pKeys,
+                                  const char *szUUID,
+                                  tABC_AccountWalletInfo *pInfo,
+                                  tABC_Error *pError);
+
+    tABC_CC ABC_AccountWalletSave(tABC_SyncKeys *pKeys,
+                                  tABC_AccountWalletInfo *pInfo,
+                                  tABC_Error *pError);
+
+    tABC_CC ABC_AccountWalletReorder(tABC_SyncKeys *pKeys,
+                                     char **aszUUID,
+                                     unsigned count,
+                                     tABC_Error *pError);
 
 #ifdef __cplusplus
 }
