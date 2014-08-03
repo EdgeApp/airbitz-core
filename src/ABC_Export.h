88b28d4b7db533e6ebaaff53c408828cbab771c7
marvtorio <marvin@airbitz.co>
2014-08-03 07:34:00 -0700

    Latests changes to export CSV feature

---
+++ src/ABC_Export.h
@@ -0,0 +1,75 @@
+/**
+ * @file
+ * AirBitz Export Data function prototypes.
+ *
+ * See LICENSE for copy, modification, and use permissions
+ *
+ * @author See AUTHORS
+ * @version 1.0
+ */
+
+#include "ABC.h"
+#include "ABC_Util.h"
+
+
+#ifndef ABC_Export_h
+#define ABC_Export_h
+
+/* Maxium size of CSV record and fields in characters */
+#define ABC_CSV_MAX_REC_SZ  4096
+#define ABC_CSV_MAX_FLD_SZ  128
+
+#define ABC_CSV_DFLT_DELIMITER "|"
+#define ABC_CSV_ALT1_DELIMITER ","
+
+#define ABC_CSV_REC_TERM_NAME "VER"
+#define ABC_CSV_REC_TERM_VALUE "1"
+
+
+#define ABC_CSV(type)    struct { \
+                                      type ov[ABC_CSV_MAX_FLD_SZ]; \
+                                      type qv[ABC_CSV_MAX_FLD_SZ]; \
+                                      size_t os; \
+                                      size_t qs; \
+                                }
+
+#define ABC_CSV_INIT(fld, src)  { \
+                                  strcpy((fld).ov, src); \
+                                  memset((fld).qv, 0, sizeof((fld).qv)); \
+                                  (fld).os = strlen(src); \
+                                  (fld).qs = 0; \
+                                }
+
+#define ABC_CSV_INIT2(fld, src, type) {\
+                                        char _tmpXform[ABC_CSV_MAX_FLD_SZ]; \
+                                        sprintf(_tmpXform,"%" type, src); \
+                                        ABC_CSV_INIT(fld, _tmpXform); \
+                                      }
+
+#define ABC_CSV_FMT(v, d)       { \
+                                  size_t _sz = 0; \
+                                  _sz = csv_write(v.qv, sizeof(v.qv), v.ov, v.os); \
+                                  v.qs = _sz; \
+                                  ABC_ALLOC(d, v.qs+1+1); \
+                                  sprintf(d, "%s%s", v.qv, ABC_CSV_DFLT_DELIMITER); \
+                                }
+
+
+typedef ABC_CSV(char) tABC_CSV;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    tABC_CC ABC_ExportFormatCsv(const char *szWalletId,
+                                const char *szStartDate,
+                                const char *szEndDate,
+                                char **szCsvData,
+                                tABC_Error *pError);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ tests/test_export.c
@@ -0,0 +1,40 @@
+#include <ABC.h>
+#include <time.h>
+#include <ABC_FileIO.h>
+#include <ABC_Sync.h>
+#include <ABC_Util.h>
+#include <csv.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+/**
+ * Performs a test sync between two repositories.
+ */
+tABC_CC TestExport(tABC_Error *pError)
+{
+    tABC_CC cc = ABC_CC_Ok;
+
+exit:
+    return cc;
+}
+
+int main()
+{
+    tABC_CC cc;
+    tABC_Error error;
+
+    cc = TestExport(&error);
+    if (cc != ABC_CC_Ok)
+    {
+        fprintf(stderr, "%s:%d: %s returned error %d (%s)\n",
+                error.szSourceFile, error.nSourceLine,
+                error.szSourceFunc, cc, error.szDescription);
+        return 1;
+    }
+
+    return 0;
+}
+
