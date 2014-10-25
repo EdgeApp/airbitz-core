8ec3328c98526706b3c86a3e73ce000bda2fd2da
William Swanson <swansontec@gmail.com>
2014-10-25 02:33:01 -0700

    Deprecate tABC_WalletInfo.szUserName

--- src/ABC.c
+++ src/ABC.c
@@ -827,6 +827,7 @@ tABC_CC ABC_GetWalletInfo(const char *szUserName,
     ABC_CHECK_ASSERT(true == gbInitialized, ABC_CC_NotInitialized, "The core library has not been initalized");
 
     ABC_CHECK_RET(ABC_WalletGetInfo(szUserName, szPassword, szUUID, ppWalletInfo, pError));
+    ABC_STRDUP((*ppWalletInfo)->szUserName, szUserName);
 
 exit:
 
@@ -941,9 +942,15 @@ tABC_CC ABC_GetWallets(const char *szUserName,
     tABC_CC cc = ABC_CC_Ok;
     ABC_SET_ERR_CODE(pError, ABC_CC_Ok);
 
+    unsigned i = 0;
+
     ABC_CHECK_ASSERT(true == gbInitialized, ABC_CC_NotInitialized, "The core library has not been initalized");
 
     ABC_CHECK_RET(ABC_WalletGetWallets(szUserName, szPassword, paWalletInfo, pCount, pError));
+    for (i = 0; i < *pCount; ++i)
+    {
+        ABC_STRDUP((*paWalletInfo)[i]->szUserName, szUserName);
+    }
 
 exit:
 
