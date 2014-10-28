6a29193e3682fb70cbaf703766a4e6fa48957b9e
William Swanson <swansontec@gmail.com>
2014-10-28 13:29:06 -0700

    Remove the TestBed project

--- src/TestBed/build/TestBed.build/Release-iphoneos/TestBed.build/Objects-normal/arm64/ABC.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamdna/Documents/DittyLabs/Clients/PaulPuey/AirBitz/TestBed/TestBed/Core/ABC.c \
-  /Users/adamdna/Documents/DittyLabs/Clients/PaulPuey/AirBitz/TestBed/TestBed/Core/ABC.h
--- src/TestBed/build/TestBed.build/Release-iphoneos/TestBed.build/Objects-normal/arm64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamdna/Documents/DittyLabs/Clients/PaulPuey/AirBitz/TestBed/TestBed/AppDelegate.m \
-  /Users/adamdna/Documents/DittyLabs/Clients/PaulPuey/AirBitz/TestBed/TestBed/AppDelegate.h
