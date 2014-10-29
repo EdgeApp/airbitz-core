ee5d648a5a84f7cfed4fcfcf61291742b262a73e
Tim Horton <tim@airbitz.co>
2014-10-28 21:14:59 -0700

    util: Add new utilities

---
+++ util/pin-login-setup.c
@@ -0,0 +1,27 @@
+#include "common.h"
+#include "ABC_LoginPassword.h"
+#include "ABC_LoginServer.h"
+#include <stdio.h>
+#include <time.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    tABC_CC cc;
+    tABC_Error error;
+    unsigned char seed[] = {1, 2, 3};
+
+    if (argc != 4)
+    {
+        fprintf(stderr, "usage: %s <dir> <user> <pass>\n", argv[0]);
+        return 1;
+    }
+    MAIN_CHECK(ABC_Initialize(argv[1], CA_CERT, seed, sizeof(seed), &error));
+
+    MAIN_CHECK(ABC_SignIn(argv[2], argv[3], NULL, NULL, &error));
+    MAIN_CHECK(ABC_PinSetup(argv[2], argv[3], &error));
+
+    return 0;
+}
