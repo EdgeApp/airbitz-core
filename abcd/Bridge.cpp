7d0c45b791855862ce85a2c686e6e69e2b6f9666
William Swanson <swansontec@gmail.com>
2015-02-23 15:26:19 -0800

    Move the transaction broadcast into its own file

--- abcd/Bridge.cpp
+++ abcd/Bridge.cpp
@@ -32,13 +32,10 @@
 #include "Bridge.hpp"
 #include "General.hpp"
 #include "Wallet.hpp"
-#include "util/Crypto.hpp"
-#include "util/Json.hpp"
-#include "util/URL.hpp"
 #include "util/Util.hpp"
+#include "bitcoin/Broadcast.hpp"
 #include "bitcoin/picker.hpp"
 #include "bitcoin/Testnet.hpp"
-#include <curl/curl.h>
 #include <wallet/wallet.hpp>
 #include <bitcoin/watcher.hpp> // Includes the rest of the stack
 #include <algorithm>
@@ -106,10 +103,6 @@ static tABC_CC     ABC_BridgeWatcherLoad(WatcherInfo *watcherInfo, tABC_Error *p
 static void        ABC_BridgeWatcherSerializeAsync(WatcherInfo *watcherInfo);
 static void        *ABC_BridgeWatcherSerialize(void *pData);
 static std::string ABC_BridgeNonMalleableTxId(bc::transaction_type tx);
-
-static tABC_CC     ABC_BridgeChainPostTx(const bc::transaction_type& tx, tABC_Error *pError);
-static tABC_CC     ABC_BridgeBlockhainPostTx(const bc::transaction_type& tx, tABC_Error *pError);
-static size_t      ABC_BridgeCurlWriteData(void *pBuffer, size_t memberSize, size_t numMembers, void *pUserData);
 
 bool check_minikey(const std::string& minikey)
 {
@@ -905,19 +898,12 @@ tABC_CC ABC_BridgeTxSignSend(tABC_TxSendInfo *pSendInfo,
         ABC_CHECK_RET(ABC_BridgeTxErrorHandler(utx, pError));
     }
 
-    // Send to chain
-    cc = ABC_BridgeChainPostTx(utx->tx, pError);
-    // If we are not on testnet and chain failed try block chain as well
-    if (!isTestnet())
+    // Send to the network:
     {
-        if (cc == ABC_CC_Ok)
-            ABC_BridgeBlockhainPostTx(utx->tx, pError);
-        else
-            cc = ABC_BridgeBlockhainPostTx(utx->tx, pError);
+        bc::data_chunk raw_tx(satoshi_raw_size(utx->tx));
+        bc::satoshi_save(utx->tx, raw_tx.begin());
+        ABC_CHECK_NEW(broadcastTx(raw_tx), pError);
     }
-
-    // Make sure the send was successful
-    ABC_CHECK_ASSERT(ABC_CC_Ok == cc, cc, "Unable to send transaction");
 
     // This will mark the outputs as spent
     watcherInfo->watcher->send_tx(utx->tx);
@@ -1319,15 +1305,11 @@ tABC_CC ABC_BridgeDoSweep(WatcherInfo *watcherInfo,
     ABC_CHECK_SYS(abcd::sign_tx(utx, keys), "sign_tx");
 
     // Send:
-    cc = ABC_BridgeChainPostTx(utx.tx, pError);
-    if (!isTestnet())
     {
-        if (cc == ABC_CC_Ok)
-            ABC_BridgeBlockhainPostTx(utx.tx, pError);
-        else
-            cc = ABC_BridgeBlockhainPostTx(utx.tx, pError);
+        bc::data_chunk raw_tx(satoshi_raw_size(utx.tx));
+        bc::satoshi_save(utx.tx, raw_tx.begin());
+        ABC_CHECK_NEW(broadcastTx(raw_tx), pError);
     }
-    ABC_CHECK_ASSERT(ABC_CC_Ok == cc, cc, "Unable to send transaction");
 
     // Save the transaction in the database:
     malTxId = bc::encode_hex(bc::hash_transaction(utx.tx));
@@ -1725,135 +1707,6 @@ static std::string ABC_BridgeNonMalleableTxId(bc::transaction_type tx)
     for (auto& input: tx.inputs)
         input.script = bc::script_type();
     return bc::encode_hex(bc::hash_transaction(tx, bc::sighash::all));
-}
-
-static
-tABC_CC ABC_BridgeChainPostTx(const bc::transaction_type& tx, tABC_Error *pError)
-{
-    tABC_CC cc = ABC_CC_Ok;
-    CURL *pCurlHandle = NULL;
-    CURLcode curlCode;
-    long resCode;
-    std::string url, resBuffer;
-    json_t *pJSON_Root = NULL;
-    char *szPut = NULL;
-
-    bc::data_chunk raw_tx(satoshi_raw_size(tx));
-    bc::satoshi_save(tx, raw_tx.begin());
-    std::string encoded(bc::encode_hex(raw_tx));
-    std::string pretty(bc::pretty(tx));
-
-    if (isTestnet())
-    {
-        url.append("https://api.chain.com/v1/testnet3/transactions");
-    }
-    else
-    {
-        url.append("https://api.chain.com/v1/bitcoin/transactions");
-    }
-
-    pJSON_Root = json_pack("{ss}", "hex", encoded.c_str());
-    szPut = ABC_UtilStringFromJSONObject(pJSON_Root, JSON_COMPACT);
-
-    ABC_DebugLog("URL: %s\n", url.c_str());
-    ABC_DebugLog("UserPwd: %s\n", CHAIN_API_USERPWD);
-    ABC_DebugLog("Body: %s\n", szPut);
-    ABC_DebugLog("\n");
-    ABC_DebugLog("%s\n", pretty.c_str());
-
-    ABC_CHECK_RET(ABC_URLCurlHandleInit(&pCurlHandle, pError))
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_URL, url.c_str())) == 0,
-        ABC_CC_Error, "Curl failed to set URL\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_USERPWD, CHAIN_API_USERPWD)) == 0,
-        ABC_CC_Error, "Curl failed to set User:Password\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_CUSTOMREQUEST, "PUT")) == 0,
-        ABC_CC_Error, "Curl failed to set Put\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_POSTFIELDS, szPut)) == 0,
-        ABC_CC_Error, "Curl failed to set post fields\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_WRITEDATA, &resBuffer)) == 0,
-        ABC_CC_Error, "Curl failed to set data\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_WRITEFUNCTION, ABC_BridgeCurlWriteData)) == 0,
-        ABC_CC_Error, "Curl failed to set callback\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_perform(pCurlHandle)) == 0,
-        ABC_CC_Error, "Curl failed to perform\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_getinfo(pCurlHandle, CURLINFO_RESPONSE_CODE, &resCode)) == 0,
-        ABC_CC_Error, "Curl failed to retrieve response info\n");
-
-    ABC_DebugLog("Chain Response Code: %d\n", resCode);
-    ABC_DebugLog("%.100s\n", resBuffer.c_str());
-    ABC_CHECK_ASSERT(resCode == 201 || resCode == 200,
-            ABC_CC_Error, "Error when sending tx to chain");
-exit:
-    if (pCurlHandle != NULL)
-    {
-        curl_easy_cleanup(pCurlHandle);
-    }
-    json_decref(pJSON_Root);
-    ABC_FREE_STR(szPut);
-
-    return cc;
-}
-
-static
-tABC_CC ABC_BridgeBlockhainPostTx(const bc::transaction_type& tx, tABC_Error *pError)
-{
-    tABC_CC cc = ABC_CC_Ok;
-    CURL *pCurlHandle = NULL;
-    CURLcode curlCode;
-    long resCode;
-    std::string url, body, resBuffer;
-
-    bc::data_chunk raw_tx(satoshi_raw_size(tx));
-    bc::satoshi_save(tx, raw_tx.begin());
-    std::string encoded(bc::encode_hex(raw_tx));
-    std::string pretty(bc::pretty(tx));
-
-    url.append("https://blockchain.info/pushtx");
-    body.append("tx=");
-    body.append(encoded);
-
-    ABC_DebugLog("%s\n", body.c_str());
-    ABC_DebugLog("\n");
-    ABC_DebugLog("%s\n", pretty.c_str());
-
-    ABC_CHECK_RET(ABC_URLCurlHandleInit(&pCurlHandle, pError))
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_URL, url.c_str())) == 0,
-        ABC_CC_Error, "Curl failed to set URL\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_POSTFIELDS, body.c_str())) == 0,
-        ABC_CC_Error, "Curl failed to set post fields\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_WRITEDATA, &resBuffer)) == 0,
-        ABC_CC_Error, "Curl failed to set data\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_setopt(pCurlHandle, CURLOPT_WRITEFUNCTION, ABC_BridgeCurlWriteData)) == 0,
-        ABC_CC_Error, "Curl failed to set callback\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_perform(pCurlHandle)) == 0,
-        ABC_CC_Error, "Curl failed to perform\n");
-    ABC_CHECK_ASSERT((curlCode = curl_easy_getinfo(pCurlHandle, CURLINFO_RESPONSE_CODE, &resCode)) == 0,
-        ABC_CC_Error, "Curl failed to retrieve response info\n");
-
-    ABC_DebugLog("Blockchain Response Code: %d\n", resCode);
-    ABC_DebugLog("%.100s\n", resBuffer.c_str());
-    ABC_CHECK_ASSERT(resCode == 200, ABC_CC_Error, "Error when sending tx to blockchain");
-exit:
-    if (pCurlHandle != NULL)
-        curl_easy_cleanup(pCurlHandle);
-
-    return cc;
-}
-
-static
-size_t ABC_BridgeCurlWriteData(void *pBuffer, size_t memberSize, size_t numMembers, void *pUserData)
-{
-    std::string *pCurlBuffer = (std::string *) pUserData;
-    unsigned int dataAvailLength = (unsigned int) numMembers * (unsigned int) memberSize;
-    size_t amountWritten = 0;
-
-    if (pCurlBuffer)
-    {
-        pCurlBuffer->append((char *) pBuffer);
-        amountWritten = dataAvailLength;
-    }
-
-    return amountWritten;
 }
 
 } // namespace abcd
