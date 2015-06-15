4ef4577d9d4c88b8b40a9651c9cb68636d60ba95
William Swanson <swansontec@gmail.com>
2015-06-15 05:06:19 -0700

    Return immediately once any broadcast succeeds
    Each broadcast runs in the background on its own detached thread.
    Once the broadcast is done, it signals a condition variable to wake up
    the main thread.
    
    If the main thread sees a successful broadcast, it returns right away.
    Otherwise, the main thread keeps waiting for some other thread to finish.
    
    All communications resources live inside reference-counted pointers,
    which allows them to continue existing even after the main broadcast
    function has exited.

--- abcd/spend/Broadcast.cpp
+++ abcd/spend/Broadcast.cpp
@@ -11,6 +11,9 @@
 #include "../crypto/Encoding.hpp"
 #include "../http/HttpRequest.hpp"
 #include "../json/JsonObject.hpp"
+#include <condition_variable>
+#include <memory>
+#include <mutex>
 #include <thread>
 
 namespace abcd {
@@ -77,27 +80,78 @@ blockchainPostTx(DataSlice tx)
     return Status();
 }
 
-template<Status (*f)(DataSlice tx)> void
-shim(Status *result, DataSlice tx)
+/**
+ * Handles a broadcast in the background.
+ * The BroadcastThread signals the condition variable when it is done.
+ * The use of `std::shared_ptr` allows the thread to continue holding
+ * resources even after the calling function has finished.
+ */
+class BroadcastThread
 {
-    *result = f(tx);
-}
+public:
+    BroadcastThread(std::shared_ptr<std::condition_variable> cv):
+        cv_(cv)
+    {}
+
+    Status status()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return status_;
+    }
+
+    bool done()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return done_;
+    }
+
+    template<Status (*f)(DataSlice tx)>
+    static void run(std::shared_ptr<BroadcastThread> self, DataChunk tx)
+    {
+        auto status = f(tx);
+        std::lock_guard<std::mutex> lock(self->mutex_);
+        self->status_ = status;
+        self->done_ = true;
+        self->cv_->notify_all();
+    }
+
+private:
+    std::shared_ptr<std::condition_variable> cv_;
+    std::mutex mutex_;
+    Status status_;
+    bool done_ = false;
+};
 
 Status
 broadcastTx(DataSlice rawTx)
 {
-    Status s1, s2, s3;
-    auto t1 = std::thread(shim<chainPostTx>, &s1, rawTx);
-    auto t2 = std::thread(shim<blockchainPostTx>, &s2, rawTx);
-    auto t3 = std::thread(shim<blockcypherPostTx>, &s3, rawTx);
+    // Create communication resources:
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    auto cv = std::make_shared<std::condition_variable>();
+    auto t1 = std::make_shared<BroadcastThread>(cv);
+    auto t2 = std::make_shared<BroadcastThread>(cv);
+    auto t3 = std::make_shared<BroadcastThread>(cv);
 
-    t1.join();
-    t2.join();
-    t3.join();
+    // Launch the broadcasts:
+    DataChunk tx(rawTx.begin(), rawTx.end());
+    std::thread(BroadcastThread::run<chainPostTx>, t1, tx).detach();
+    std::thread(BroadcastThread::run<blockchainPostTx>, t2, tx).detach();
+    std::thread(BroadcastThread::run<blockcypherPostTx>, t3, tx).detach();
 
-    if (s1) return s1;
-    if (s2) return s2;
-    return s3;
+    // Loop as long as any thread is still running:
+    while (!t1->done() || !t2->done() || !t3->done())
+    {
+        cv->wait(lock);
+        // Quit immediately if one has succeeded:
+        if ((t1->done() && t1->status()) ||
+            (t2->done() && t2->status()) ||
+            (t3->done() && t3->status()))
+            return Status();
+    }
+
+    // We only get here if all three have failed:
+    return t3->status();
 }
 
 } // namespace abcd
