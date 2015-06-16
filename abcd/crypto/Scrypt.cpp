9bf26aac035b19a17b41c1f62d8680ddaf547424
William Swanson <swansontec@gmail.com>
2015-06-16 12:49:16 -0700

    Move JsonSnrp into its own file

--- abcd/crypto/Scrypt.cpp
+++ abcd/crypto/Scrypt.cpp
@@ -6,12 +6,10 @@
  */
 
 #include "Scrypt.hpp"
-#include "Encoding.hpp"
 #include "Random.hpp"
 #include "../bitcoin/Testnet.hpp"
 #include "../../minilibs/scrypt/crypto_scrypt.h"
 #include <sys/time.h>
-#include <memory>
 
 namespace abcd {
 
@@ -151,49 +149,6 @@ usernameSnrp()
     };
 
     return isTestnet() ? testnet : mainnet;
-}
-
-Status
-JsonSnrp::snrpGet(ScryptSnrp &result) const
-{
-    ABC_CHECK(saltOk());
-    ABC_CHECK(nOk());
-    ABC_CHECK(rOk());
-    ABC_CHECK(pOk());
-
-    ABC_CHECK(base16Decode(result.salt, salt()));
-    result.n = n();
-    result.r = r();
-    result.p = p();
-    return Status();
-}
-
-Status
-JsonSnrp::snrpSet(const ScryptSnrp &snrp)
-{
-    ABC_CHECK(saltSet(base16Encode(snrp.salt).c_str()));
-    ABC_CHECK(nSet(snrp.n));
-    ABC_CHECK(rSet(snrp.r));
-    ABC_CHECK(pSet(snrp.p));
-    return Status();
-}
-
-Status
-JsonSnrp::create()
-{
-    ScryptSnrp snrp;
-    ABC_CHECK(snrp.create());
-    ABC_CHECK(snrpSet(snrp));
-    return Status();
-}
-
-Status
-JsonSnrp::hash(DataChunk &result, DataSlice data) const
-{
-    ScryptSnrp snrp;
-    ABC_CHECK(snrpGet(snrp));
-    ABC_CHECK(snrp.hash(result, data));
-    return Status();
 }
 
 } // namespace abcd
--- abcd/crypto/Scrypt.hpp
+++ abcd/crypto/Scrypt.hpp
@@ -8,7 +8,6 @@
 #ifndef ABCD_CRYPTO_SCRYPT_HPP
 #define ABCD_CRYPTO_SCRYPT_HPP
 
-#include "../json/JsonObject.hpp"
 #include "../util/Data.hpp"
 #include "../util/Status.hpp"
 
@@ -50,32 +49,6 @@ struct ScryptSnrp
  */
 const ScryptSnrp &
 usernameSnrp();
-
-/**
- * Serialization format.
- */
-struct JsonSnrp:
-    public JsonObject
-{
-    ABC_JSON_CONSTRUCTORS(JsonSnrp, JsonObject)
-
-    ABC_JSON_STRING(salt, "salt_hex", nullptr)
-    ABC_JSON_INTEGER(n, "n", 0)
-    ABC_JSON_INTEGER(r, "r", 0)
-    ABC_JSON_INTEGER(p, "p", 0)
-
-    Status
-    snrpGet(ScryptSnrp &result) const;
-
-    Status
-    snrpSet(const ScryptSnrp &snrp);
-
-    Status
-    create();
-
-    Status
-    hash(DataChunk &result, DataSlice data) const;
-};
 
 } // namespace abcd
 
