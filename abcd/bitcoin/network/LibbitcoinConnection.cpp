23a060ecc5d618c84feecb6905803fe09a2ed798
William Swanson <swansontec@gmail.com>
2016-04-28 06:52:49 -0700

    Create a `LibbitcoinConnection` class
    This class implements the new `IBitcoinConnection` interface,
    and will help provide common ground between libbitcoin and Stratum.

---
+++ abcd/bitcoin/network/LibbitcoinConnection.cpp
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2016, Airbitz, Inc.
+ * All rights reserved.
+ *
+ * See the LICENSE file for more information.
+ */
+
+#include "LibbitcoinConnection.hpp"
+
+namespace abcd {
+
+static void
+onUnknownNop(const std::string &)
+{
+}
+
+LibbitcoinConnection::LibbitcoinConnection(void *ctx):
+    queuedQueries_(0),
+    socket_(ctx),
+    codec_(socket_, onUnknownNop, std::chrono::seconds(10), 0)
+{
+}
+
+Status
+LibbitcoinConnection::connect(const std::string &uri, const std::string &key)
+{
+    uri_ = uri;
+
+    if(!socket_.connect(uri_, key))
+        return ABC_ERROR(ABC_CC_Error, "Could not connect to " + uri_);
+
+    return Status();
+}
+
+bc::client::sleep_time
+LibbitcoinConnection::wakeup()
+{
+    bc::client::sleep_time nextWakeup(0);
+    auto now = std::chrono::steady_clock::now();
+
+    // Figure out when our next block check is:
+    if (heightCallback_)
+    {
+        auto period = std::chrono::seconds(30);
+        auto elapsed = std::chrono::duration_cast<bc::client::sleep_time>(
+                           now - lastHeightCheck_);
+        if (period <= elapsed)
+        {
+            fetchHeight();
+            lastHeightCheck_ = now;
+            elapsed = bc::client::sleep_time::zero();
+        }
+        nextWakeup = period - elapsed;
+    }
+
+    // Handle the socket:
+    socket_.forward(codec_);
+    nextWakeup = bc::client::min_sleep(nextWakeup, codec_.wakeup());
+
+    return nextWakeup;
+}
+
+zmq_pollitem_t
+LibbitcoinConnection::pollitem()
+{
+    return socket_.pollitem();
+}
+
+std::string
+LibbitcoinConnection::uri()
+{
+    return uri_;
+}
+
+bool
+LibbitcoinConnection::queueFull()
+{
+    return 10 < queuedQueries_;
+}
+
+void
+LibbitcoinConnection::heightSubscribe(const StatusCallback &onError,
+                                      const HeightCallback &onReply)
+{
+    heightError_ = onError;
+    heightCallback_ = onReply;
+    lastHeightCheck_ = std::chrono::steady_clock::now();
+
+    fetchHeight();
+}
+
+void
+LibbitcoinConnection::addressHistoryFetch(const StatusCallback &onError,
+        const AddressCallback &onReply,
+        const std::string &address)
+{
+    bc::payment_address parsed;
+    if (!parsed.set_encoded(address))
+        return onError(ABC_ERROR(ABC_CC_ParseError, "Bad address " + address));
+
+    auto errorShim = [this, onError](const std::error_code &error)
+    {
+        --queuedQueries_;
+        onError(ABC_ERROR(ABC_CC_Error, error.message()));
+    };
+
+    auto replyShim = [this, onReply](const bc::client::history_list &history)
+    {
+        --queuedQueries_;
+
+        AddressHistory historyOut;
+        for (const auto &row: history)
+        {
+            historyOut[bc::encode_hash(row.output.hash)] = row.output_height;
+            if (row.spend.hash != bc::null_hash)
+                historyOut[bc::encode_hash(row.spend.hash)] = row.spend_height;
+        }
+        onReply(historyOut);
+    };
+
+    ++queuedQueries_;
+    codec_.address_fetch_history(errorShim, replyShim, parsed);
+}
+
+void
+LibbitcoinConnection::txDataFetch(const StatusCallback &onError,
+                                  const TxCallback &onReply,
+                                  const std::string &txid)
+{
+    bc::hash_digest parsed;
+    if (!bc::decode_hash(parsed, txid))
+        return onError(ABC_ERROR(ABC_CC_ParseError, "Bad txid " + txid));
+
+    auto errorShim = [this, onError](const std::error_code &error)
+    {
+        --queuedQueries_;
+        onError(ABC_ERROR(ABC_CC_Error, error.message()));
+    };
+
+    auto replyShim = [this, onReply](const bc::transaction_type &tx)
+    {
+        --queuedQueries_;
+        onReply(tx);
+    };
+
+    auto onErrorRetry = [this, errorShim, replyShim, parsed]
+                        (const std::error_code &error)
+    {
+        // If that didn't work, try the mempool:
+        codec_.fetch_unconfirmed_transaction(errorShim, replyShim, parsed);
+    };
+
+    ++queuedQueries_;
+    codec_.fetch_transaction(onErrorRetry, replyShim, parsed);
+}
+
+void
+LibbitcoinConnection::fetchHeight()
+{
+    auto errorShim = [this](const std::error_code &error)
+    {
+        --queuedQueries_;
+        heightError_(ABC_ERROR(ABC_CC_Error, error.message()));
+    };
+
+    auto replyShim = [this](size_t height)
+    {
+        --queuedQueries_;
+        heightCallback_(height);
+    };
+
+    ++queuedQueries_;
+    codec_.fetch_last_height(errorShim, replyShim);
+}
+
+} // namespace abcd
---
+++ abcd/bitcoin/network/LibbitcoinConnection.hpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016, Airbitz, Inc.
+ * All rights reserved.
+ *
+ * See the LICENSE file for more information.
+ */
+
+#ifndef ABCD_BITCOIN_NETWORK_LIBBITCOIN_CONNECTION_HPP
+#define ABCD_BITCOIN_NETWORK_LIBBITCOIN_CONNECTION_HPP
+
+#include "IBitcoinConnection.hpp"
+#include "../../../minilibs/libbitcoin-client/client.hpp"
+
+namespace abcd {
+
+/**
+ * Wraps a libbitcoin connection in the `IBitcoinConnection` interface.
+ */
+class LibbitcoinConnection:
+    public bc::client::sleeper,
+    public IBitcoinConnection
+{
+public:
+    LibbitcoinConnection(void *ctx);
+
+    Status connect(const std::string &uri, const std::string &key);
+    zmq_pollitem_t pollitem();
+
+    // Sleeper interface:
+    bc::client::sleep_time wakeup() override;
+
+    // IBitcoinConnection interface:
+    std::string
+    uri() override;
+
+    bool
+    queueFull() override;
+
+    void
+    heightSubscribe(const StatusCallback &onError,
+                    const HeightCallback &onReply) override;
+
+    void
+    addressHistoryFetch(const StatusCallback &onError,
+                        const AddressCallback &onReply,
+                        const std::string &address) override;
+
+    void
+    txDataFetch(const StatusCallback &onError,
+                const TxCallback &onReply,
+                const std::string &txid) override;
+
+private:
+    // Connection:
+    std::string uri_;
+    int queuedQueries_;
+
+    // Height-check state:
+    StatusCallback heightError_;
+    HeightCallback heightCallback_;
+    std::chrono::steady_clock::time_point lastHeightCheck_;
+
+    // The actual obelisk connection (destructor called first):
+    bc::client::zeromq_socket socket_;
+    bc::client::obelisk_codec codec_;
+
+    void
+    fetchHeight();
+};
+
+} // namespace abcd
+
+#endif
