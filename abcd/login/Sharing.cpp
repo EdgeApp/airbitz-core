196ad87b47c9ed35ef3ad4a1229ee4c24be52cf1
William Swanson <swansontec@gmail.com>
2017-02-24 17:36:40 -0800

    Move the lobby ECDH code into its own function

--- abcd/login/Sharing.cpp
+++ abcd/login/Sharing.cpp
@@ -48,6 +48,44 @@ struct LobbyJson:
     ABC_JSON_VALUE(accountRequest, "accountRequest", AccountRequestJson)
 };
 
+/**
+ * Given the request's public key,
+ * create an encryption key and corresponding public key.
+ */
+static Status
+makeKeys(DataChunk &result, std::string &replyPubkey,
+         const std::string &requestPubkey)
+{
+    // Make an ephemeral private key:
+    bc::ec_secret replyKey;
+    do
+    {
+        DataChunk random;
+        ABC_CHECK(randomData(random, replyKey.size()));
+        std::copy(random.begin(), random.end(), replyKey.begin());
+    }
+    while (!bc::verify_private_key(replyKey));
+
+    // Derive the secret X coordinate via ECDH:
+    bc::ec_point requestKey;
+    ABC_CHECK(base16Decode(requestKey, requestPubkey));
+    if (!bc::ec_multiply(requestKey, replyKey))
+        return ABC_ERROR(ABC_CC_EncryptError, "Lobby ECDH error");
+
+    // The secret X coordinate, in big-endian format:
+    const auto secretX = DataChunk(requestKey.begin() + 1,
+                                   requestKey.begin() + 33);
+
+    // We could have used the KDF from NIST.SP.800-56Ar2 section 5.8.1,
+    // `hmac(0x00 | 0x00 | 0x00 | 0x01 | x | otherInfo, salt)`,
+    // but this is similar, and we are alredy using it:
+    const auto dataKey = hmacSha256(std::string("dataKey"), secretX);
+
+    replyPubkey = base16Encode(bc::secret_to_public_key(replyKey));
+    result = dataKey;
+    return Status();
+}
+
 Status
 lobbyFetch(Lobby &result, const std::string &id)
 {
@@ -81,24 +119,10 @@ loginRequestApprove(Login &login,
     ABC_CHECK(requestJson.requestKeyOk());
     ABC_CHECK(requestJson.typeOk());
 
-    // Make an ephemeral private key:
-    bc::ec_secret replyKey;
-    do
-    {
-        DataChunk random;
-        ABC_CHECK(randomData(random, replyKey.size()));
-        std::copy(random.begin(), random.end(), replyKey.begin());
-    }
-    while (!bc::verify_private_key(replyKey));
-
-    // Derive the encryption key via ECDH:
-    bc::ec_point requestKey;
-    ABC_CHECK(base16Decode(requestKey, requestJson.requestKey()));
-    if (!bc::ec_multiply(requestKey, replyKey))
-        return ABC_ERROR(ABC_CC_EncryptError, "Lobby ECDH error");
-    const auto secret = DataChunk(requestKey.begin() + 1,
-                                  requestKey.begin() + 33);
-    const auto dataKey = hmacSha256(std::string("dataKey"), secret);
+    // Create the encryption key:
+    DataChunk dataKey;
+    std::string replyPubkey;
+    ABC_CHECK(makeKeys(dataKey, replyPubkey, requestJson.requestKey()));
 
     // Get the repo info we need:
     RepoInfo repoInfo;
@@ -120,8 +144,7 @@ loginRequestApprove(Login &login,
 
     // Update the lobby JSON:
     ABC_CHECK(requestJson.replyBoxSet(replyBox));
-    ABC_CHECK(requestJson.replyKeySet(base16Encode(
-                                          bc::secret_to_public_key(replyKey))));
+    ABC_CHECK(requestJson.replyKeySet(replyPubkey));
 
     // Upload:
     ABC_CHECK(loginServerLobbySet(lobby.id, lobby.json));
